04b31e7790bc07dd278d3f029887ed785449efca
Amusa Abayomi <amusaabayomipaul99@gmail.com>
2021-06-12 20:08:14 +0100

    Lesson files of how to generate a secured token in php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>How to generate a secured token in php</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="lesson-title">
+        <div class="lesson-title-inner">
+            <h3>How to generate a Secured token in PHP</h3>
+        </div>
+    </div>
+    <?php
+        $generated_token = openssl_random_pseudo_bytes(30, $safe);
+        $converted_token = bin2hex($generated_token);
+        $isSafe = $safe == 1 ? 'true' : 'false';
+
+        $getRandomString = function($string, $len){
+            $randString = '';
+            for ($i=0; $i < $len; $i++) { 
+                $randString .= $string[rand(0, $len - 1)];
+            }
+
+            return $randString;
+        }
+
+    ?>
+    <div class="lesson-body">
+        <div class="lesson-inner">
+            <div class="lesson-left">Full Token</div>
+            <div class="lesson-right"><?php echo $converted_token; ?></div>
+        </div>
+        <div class="lesson-inner">
+            <div class="lesson-left">Token (10 Random Chars)</div>
+            <div class="lesson-right"><?php echo $getRandomString($converted_token, 10); ?></div>
+        </div>
+        <div class="lesson-inner">
+            <div class="lesson-left">is Secured</div>
+            <div class="lesson-right"><?php echo $isSafe ?></div>
+        </div>
+    </div>
+</body>
+</html>
